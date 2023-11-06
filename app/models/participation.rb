f479b919a9a84f6e3f930644be825bbd6f9b0b3e
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-06 19:21:51 +0100

    To add admin infos to user, I use a boolean and made a new migration for that

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,2 +1,7 @@
 class Participation < ApplicationRecord
+  after_create :attendance_send
+
+  def attendance_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'events/index'
+  get 'events/show'
+  get 'events/new'
+  get 'events/create'
+  get 'events/edit'
+  get 'events/update'
+  get 'events/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
+  root to: 'events#index'
+  resources :attendances
+  resources :events
+  resources :users
 end
